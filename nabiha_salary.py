d3ba307e4b2a9e70c8dc49e1133211db9b5db73f
nourhankir <nourhankir@hotmail.com>
2025-02-19 19:59:38 +0200

    adding dictionary

--- nabiha_salary.py
+++ nabiha_salary.py
@@ -20,46 +20,139 @@ The script should calculate and display:
 
 Finally, the script should output all the results in a readable format.
 '''
-
+additional_savings=0
+months=[]
+budget=[]
 import random
+savings_per=int(input('enter the percentage for \nsavings: '))
+rent_per=int(input('rent:'))
+electricity_per=int(input('electricity: '))
 while True:
+    #1
     salary=int(input('Nabiha al kabiha please enter your salary: '))
-    month=input('Nabiha input the name of the month she is storing the salary for: ')
-    savings_per=int(input('enter the percentage for \nsavings: '))
-    rent_per=int(input('rent:'))
-    electricity_per=int(input('electricity: '))
-    saving=(salary*savings_per)/100
-    rent=(salary*rent_per)/100
-    electricity=(salary*electricity_per)/100
-    total_spending=saving+rent+electricity
-    remainder=salary-total_spending
-    yearly_rent=rent*12
-    yearly_electricity=electricity*12
-    double_salary=salary**2
-    additional_savings=random.randint(10,100)
-    if saving!=0:
-        savings_division = additional_savings / saving 
-    else:
-        savings_division=0
-    print("\nFinancial Summary for", month)
-    print("-" * 40)
-    print(f"Total Salary: ${salary}")
-    print(f"Allocated to Savings: ${saving}")
-    print(f"Allocated to Rent: ${rent}")
-    print(f"Allocated to Electricity: ${electricity}")
-    print(f"Total Expenses: ${total_spending}")
-    print(f"Remaining Salary: ${remainder}")
-    print(f"Estimated Yearly Rent: ${yearly_rent}")
-    print(f"Estimated Yearly Electricity Cost: ${yearly_electricity}")
-    print(f"Salary Squared (for fun): ${double_salary}")
-    print(f"you have an extra {savings_division}% on your saving ")
-    print("-" * 40)
-    cont=input('do you want to continue?')
-    if cont=='no':
-        break
-    
-
-
+    #2
+    month_name=input('Nabiha input the name of the month she is storing the salary for: ')
+    if month_name not in months:
+        #3
+        saving=(salary*savings_per)/100
+        #4
+        rent=(salary*rent_per)/100
+        #5
+        electricity=(salary*electricity_per)/100
+        #6
+        total_spending=saving+rent+electricity
+        #7
+        remainder=salary-total_spending
+        #8
+        yearly_rent=rent*12
+        #9
+        yearly_electricity=electricity*12
+        #10
+        double_salary=salary**2
+        month={"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
+            'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings}
+        months.append(month_name)
+        budget.append(month)
+        #11
         
+        if saving!=0:
+            savings_division = additional_savings / saving 
+        else:
+            savings_division=0
+        print("\nFinancial Summary for", month)
+        print("-" * 40)
+        print(f"Total Salary: ${salary}")
+        print(f"Allocated to Savings: ${saving}")
+        print(f"Allocated to Rent: ${rent}")
+        print(f"Allocated to Electricity: ${electricity}")
+        print(f"Total Expenses: ${total_spending}")
+        print(f"Remaining Salary: ${remainder}")
+        print(f"Estimated Yearly Rent: ${yearly_rent}")
+        print(f"Estimated Yearly Electricity Cost: ${yearly_electricity}")
+        print(f"Salary Squared (for fun): ${double_salary}")
+        print(f"you have an extra {savings_division}% on your saving ")
+        print("-" * 40)
+        cont=input('do you want to continue?')
+        if cont=='no':
+            break
+        
+    else:
+        #3
+        saving=(salary*savings_per)/100
+        #4
+        rent=(salary*rent_per)/100
+        #5
+        electricity=(salary*electricity_per)/100
+        #6
+        total_spending=saving+rent+electricity
+        #7
+        remainder=salary-total_spending
+        #8
+        yearly_rent=rent*12
+        #9
+        yearly_electricity=electricity*12
+        #10
+        double_salary=salary**2
+        for dic in budget:
+            if dic['month_name']==month_name:
+                dic.update({"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
+                    'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings})
+                print("\nFinancial Summary for", month)
+        print("-" * 40)
+        print(f"Total Salary: ${salary}")
+        print(f"Allocated to Savings: ${saving}")
+        print(f"Allocated to Rent: ${rent}")
+        print(f"Allocated to Electricity: ${electricity}")
+        print(f"Total Expenses: ${total_spending}")
+        print(f"Remaining Salary: ${remainder}")
+        print(f"Estimated Yearly Rent: ${yearly_rent}")
+        print(f"Estimated Yearly Electricity Cost: ${yearly_electricity}")
+        print(f"Salary Squared (for fun): ${double_salary}")
+        print(f"you have an extra {savings_division}% on your saving ")
+        print("-" * 40)
+        cont=input('do you want to continue?')
+        if cont=='no':
+            break    
+print('-'*40)
+additional_savings=int(input('Enter your additional savings'))
+month_name=input('enter the month where you had an additional saving')
+salary=salary+additional_savings
+#3
+saving=(salary*savings_per)/100
+#4
+rent=(salary*rent_per)/100
+#5
+electricity=(salary*electricity_per)/100
+#6
+total_spending=saving+rent+electricity
+#7
+remainder=salary-total_spending
+#8
+yearly_rent=rent*12
+#9
+yearly_electricity=electricity*12
+#10
+double_salary=salary**2
+if saving!=0:
+    savings_division = additional_savings / saving 
+else:
+    savings_division=0
+for dic in budget:
+    if dic['month_name']==month_name:
+        dic.update({"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
+            'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings})
+print("\nFinancial Summary for", month)
+print("-" * 40)
+print(f"Total Salary: ${salary}")
+print(f"Allocated to Savings: ${saving}")
+print(f"Allocated to Rent: ${rent}")
+print(f"Allocated to Electricity: ${electricity}")
+print(f"Total Expenses: ${total_spending}")
+print(f"Remaining Salary: ${remainder}")
+print(f"Estimated Yearly Rent: ${yearly_rent}")
+print(f"Estimated Yearly Electricity Cost: ${yearly_electricity}")
+print(f"Salary Squared (for fun): ${double_salary}")
+print(f"you have an extra {savings_division}% on your saving ")
+print("-" * 40)
 
-    
+            
