3c9384bf6ac46c9c41bf6ed8eb7ee388736444a4
nourhankir <nourhankir@hotmail.com>
2025-02-21 20:52:51 +0200

    adding a menu system

--- nabiha_salary.py
+++ nabiha_salary.py
@@ -1,25 +1,4 @@
-'''
-Nabiha, a software engineering consultant, receives a variable salary every month. She wants to create a Python script that helps her manage her monthly finances by calculating how much money will be allocated to different categories like savings, rent, and electricity, based on percentages of his salary.
 
-Your task is to write a Python script that automates these calculations. The script should:
-
-
-• Ask Nabiha to input her salary for the month.
-• Ask Nabiha to input the name of the month she is storing the salary for.
-• Ask Nabiha to input the following percentages for: a) Savings b) Rent c) Electricity
-
-The script should calculate and display:
- 
-
-• The amount allocated to savings, rent, and electricity.
-• The total amount Nabiha spends on savings, rent, and electricity combined.
-• The remainder of Nabiha’s salary after these expenses.
-• The monthly rent and electricity multiplied by 12 to estimate Nabiha's yearly rent and electricity costs.
-• Nabiha's total salary for the month raised to the power of 2 (just for fun).
-• Assume Nabiha saves an additional random amount (e.g., $50) each month, and you need to calculate how much would be left if this amount is divided by the total amount allocated to savings. 
-
-Finally, the script should output all the results in a readable format.
-'''
 def printing(month,salary,saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary,savings_division):
         print("\nFinancial Summary for", month)
         print("-" * 40)
@@ -56,60 +35,107 @@ def calculation(salary,savings_per,rent_per,electricity_per):
 additional_savings=0
 months=[]
 budget=[]
-import random
+
+def menu():
+    print('1)input your salary')
+    print('2)edit your month salary')
+    print('3)add an additional amount of money')
+    print('4)read your schedule for the month')
+    print('5)Exit')
+    
+print('hi Samira this is a program to help you manage your monthly finances ')
 savings_per=int(input('enter the percentage for \nsavings: '))
 rent_per=int(input('rent:'))
 electricity_per=int(input('electricity: '))
+menu()
 while True:
-    #1
-    salary=int(input('Nabiha al kabiha please enter your salary: '))
-    #2
-    month_name=input('Nabiha input the name of the month she is storing the salary for: ')
-    if month_name not in months:
-        saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary=calculation(salary,savings_per,rent_per,electricity_per)
-        month={"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
-            'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings}
-        months.append(month_name)
-        budget.append(month)
-        #11
+    action=input('enter the action you would like to take ')
+    if action in ['1','2','3','4','5']:
+        break
+while True:
+    if action=='1':
+        month_name=input('Nabiha input the name of the month she is storing the salary for: ')
+        salary=int(input('Nabiha al kabiha please enter your salary: '))
         
-        if saving!=0:
-            savings_division = additional_savings / saving 
+        if month_name not in months:
+            saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary=calculation(salary,savings_per,rent_per,electricity_per)
+            month={"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
+                'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings}
+            months.append(month_name)
+            budget.append(month)
+            
+            if saving!=0:
+                savings_division = additional_savings / saving 
+            else:
+                savings_division=0
         else:
-            savings_division=0
-        printing(month,salary,saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary,savings_division)
-        cont=input('do you want to continue?')
-        if cont=='no':
-            break
-        
-    else:
+            print('you added this month already')
+            menu()
+            while True:
+                action=input('enter the action you would like to take ')
+                if action in ['1','2','3','4','5']:
+                    break
+        menu()
+        while True:
+            action=input('enter the action you would like to take ')
+            if action in ['1','2','3','4','5']:
+                break
+    if action=='2':
+        month_name=input('Enter the name of the month you would like to edit: ')
+        salary=int(input('Enter the new salary'))
         saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary=calculation(salary,savings_per,rent_per,electricity_per)
         for dic in budget:
             if dic['month_name']==month_name:
                 dic.update({"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
                     'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings})
-                print("\nFinancial Summary for", month)
-        printing(month,salary,saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary,savings_division)
-        cont=input('do you want to continue?')
-        if cont=='no':
-            break    
-print('-'*40)
-month_name=input('enter the month where you had an additional saving: ')
+        menu()
+        while True:
+            action=input('enter the action you would like to take ')
+            if action in ['1','2','3','4','5']:
+                break        
+                       
+    if action=='3':
+        month_name=input('enter the month where you had an additional saving: ')
 
-additional_savings=int(input('Enter your additional savings: '))
+        additional_savings=int(input('Enter your additional savings: '))
 
-salary=salary+additional_savings
-saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary=calculation(salary,savings_per,rent_per,electricity_per)
-if saving!=0:
-    savings_division = additional_savings / saving 
-else:
-    savings_division=0
-for dic in budget:
-    if dic['month_name']==month_name:
-        dic.update({"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
-            'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings})
-printing(month,salary,saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary,savings_division)
-
-
+        salary=salary+additional_savings
+        saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary=calculation(salary,savings_per,rent_per,electricity_per)
+        if saving!=0:
+            savings_division = additional_savings / saving 
+        else:
+            savings_division=0
+        for dic in budget:
+            if dic['month_name']==month_name:
+                dic.update({"month_name":month_name,'salary':salary,"saving":saving,'rent':rent,'electricity':electricity,'total_spending':total_spending,
+                    'remainder':remainder,'yearly_rent':yearly_rent,'yearly_electricity':yearly_electricity,'double_salary':double_salary,'additional_savings':additional_savings})
+        menu()
+        while True:
+            action=input('enter the action you would like to take ')
+            if action in ['1','2','3','4','5']:
+                break 
+    if action=='4':
+        month_name=input('enter the name of the month you want to see: ')
+        for dic in budget:
+            if dic['month_name']==month_name:
+                month_name=dic['month_name']
+                salary=dic['salary']
+                saving=dic['saving']
+                rent=dic['rent']
+                electricity=dic['electricity']
+                total_spending=dic['total_spending']
+                remainder=dic['remainder']
+                yearly_rent=dic['yearly_rent']
+                yearly_electricity=dic['yearly_electricity']
+                double_salary=dic['double_salary']
+                additional_savings=dic['additional_savings']
+                printing(month,salary,saving,rent,electricity,total_spending,remainder,yearly_rent,yearly_electricity,double_salary,savings_division)
+                menu()
+                while True:
+                    action=input('enter the action you would like to take ')
+                    if action in ['1','2','3','4','5']:
+                        break         
+    if action=='5': 
+        break                  
 
             
